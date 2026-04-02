0ef7a4393d9f3de6872a95fc2b3a95dcdc0a3da2
Ranvijay Tiwari <rt7999675@gmail.com>
2026-04-03 03:01:10 +0530

    feat(ui): Setup global API configs and application core styling assets

---
+++ frontend/src/frontend-config/api.js
@@ -0,0 +1,9 @@
+export const BACKEND_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8001/api/v1"
+    : "https://course-selling-app-r3sw.onrender.com/api/v1";
+
+
+  
+  
+   
