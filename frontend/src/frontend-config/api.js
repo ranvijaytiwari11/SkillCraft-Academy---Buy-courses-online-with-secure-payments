9ecef57b2fb6d0771af48f85e81266effbae9702
ranvijaytiwari11 <208862231+ranvijaytiwari11@users.noreply.github.com>
2026-04-06 20:43:34 +0530

    Update api.js

--- frontend/src/frontend-config/api.js
+++ frontend/src/frontend-config/api.js
@@ -2,7 +2,7 @@ export const BACKEND_URL =
   import.meta.env.VITE_BACKEND_URL ||
   (import.meta.env.MODE === "development"
     ? "http://localhost:8001/api/v1"
-    : "https://course-selling-app-r3sw.onrender.com/api/v1");
+    : "https://skill-craft-academy-buy-courses-onl.vercel.app");
 
 
   
